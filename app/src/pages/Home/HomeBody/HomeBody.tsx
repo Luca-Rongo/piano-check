a5094247d2ebfa9f3578599e41a0866a5653c604
Luca-Rongo <113420364+Luca-Rongo@users.noreply.github.com>
2024-05-31 01:40:45 +0200

    HomeBody  and HomeHeader Created

---
+++ app/src/pages/Home/HomeBody/HomeBody.tsx
@@ -0,0 +1,113 @@
+import {
+  Container,
+  VStack,
+  Card,
+  CardBody,
+  Center,
+  Text,
+  Image,
+  CardFooter,
+  Box,
+} from "@chakra-ui/react";
+import { transform } from "framer-motion";
+import { useRef, useEffect, useState } from "react";
+import { BsFileEarmarkMusic } from "react-icons/bs";
+import { FaMusic } from "react-icons/fa";
+import { parseString } from "xml2js";
+import { asserts, elements, MusicXML } from "@stringsync/musicxml";
+import { OpenSheetMusicDisplay } from "opensheetmusicdisplay";
+
+export default function HomeBody({ xml }: { xml: any }) {
+  return (
+    <>
+      {!xml ? (
+        <Container margin={0} position={"absolute"} top={"40%"}>
+          <Center>
+            <Card h={150}>
+              <CardBody marginTop={10}>
+                <Text opacity={0.5}>Select a sheet and a recording</Text>
+              </CardBody>
+            </Card>
+          </Center>
+          {/* <Image src="sheet.jpg" alt="HeadPhones" boxSize={200} margin={0} opacity={0.5}/> */}
+        </Container>
+      ) : (
+        <MusicSheet xml={xml} />
+      )}
+    </>
+  );
+}
+
+function MusicSheet({ xml }: { xml: any }) {
+  console.log(xml);
+  // const decompressedXml = fs.createReadStream(); 
+  const parser = parseString(xml, (err, result) => {
+    console.log(result);
+    console.log(err)
+  });
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [xmlParsed, setXmlParsed] = useState<any>(null);
+  useEffect(() => {
+    const musicXml = MusicXML.parse(xml);
+    console.log(musicXml);
+    const osmd = new OpenSheetMusicDisplay(canvasRef.current!);
+    osmd.setOptions({
+      backend: "svg",
+      drawTitle: true,
+    });
+    osmd.load(xml).then(() => {
+      osmd.render();
+    });
+    console.log(musicXml);
+    setXmlParsed(musicXml);
+  });
+
+  return (
+    <Container>
+      <canvas ref={canvasRef}></canvas>
+    </Container>
+  );
+}
+
+// const convertToVexFlowNotes = (data: any) => {
+//   const notes = data["score-partwise"]["part"][0]["measure"][0]["note"];
+//   return notes.map(({ note }: { note: any }) => {
+//     const pitch = note["pitch"][0];
+//     const step = pitch["step"][0];
+//     const octave = pitch["octave"][0];
+//     const duration = note["duration"][0];
+//     const noteName = `${step}/${octave}`;
+//     return new StaveNote({
+//       keys: [noteName],
+//       duration: duration,
+//     });
+//   });
+// };
+
+// useEffect(() => {
+//   const renderMusicXml = async () => {
+//     try {
+//       const parsedData = await parseXml(xml);
+//       const notes = convertToVexFlowNotes(parsedData);
+//       const VF = Vex.Flow;
+//       const div = divRef.current;
+//       const renderer = new VF.Renderer(div!, VF.Renderer.Backends.SVG);
+
+//       renderer.resize(500, 200);
+//       const context = renderer.getContext();
+//       context.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
+
+//       const stave = new VF.Stave(10, 40, 400);
+//       const voice = new VF.Voice({ num_beats: 4, beat_value: 4 });
+//       voice.addTickables(notes);
+
+//       const formatter = new VF.Formatter()
+//         .joinVoices([voice])
+//         .format([voice], 400);
+//       voice.draw(context, stave);
+//     } catch (error) {
+//       console.error("Error rendering MusicXML:", error);
+//     }
+//   };
+//   renderMusicXml()
+// }, [xml]);
---
+++ app/src/pages/Home/HomeHeader/HomeHeader.tsx
@@ -0,0 +1,129 @@
+import { FilePicker } from "@capawesome/capacitor-file-picker";
+import { Search2Icon } from "@chakra-ui/icons";
+import {
+  Container,
+  Center,
+  ButtonGroup,
+  WrapItem,
+  Button,
+  Icon,
+} from "@chakra-ui/react";
+import { BoundFunction } from "@testing-library/react";
+import { useCallback, useRef, useState } from "react";
+import zlib from "node:zlib";
+import fs from "fs";
+import unzipper from "unzipper";
+export default function HomeHeader({ setXml }: { setXml: any }) {
+  const [xmlFile, setXmlFile] = useState<any>("");
+  const [AudioFile, setAudioFile] = useState<string | null>("");
+  const xmlFileElement = useRef<HTMLInputElement | null>(null);
+  const audioFileElement = useRef<HTMLInputElement | null>(null);
+
+  const handleXmlFileChange = (event: any) => {
+    const file = event.target.files[0];
+    console.log(event.target.files);
+    if (file) {
+      const fileContent = fs.createReadStream(`./${file.name}`);
+      // .pipe(unzipper.Extract({ path: "output/path" }));
+
+      // .pipe(unzipper.Parse({ forceStream: true }));
+      // const reader = new FileReader();
+      // reader.onload = (e) => {
+      //   setXmlFile(e.target!.result);
+      //   const unzip =  zlib.createGunzip()
+      //   // setXml(e.target!.result);
+      //   // console.log(e.target!.result);
+      // };
+      // reader.readAsText(file);
+      // setXml(file);
+    }
+    console.log("file", file);
+  };
+
+  const handleAudioFileChange = (event: any) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        setAudioFile(e.target!.result as string);
+      };
+      reader.readAsText(file);
+    }
+    console.log("file", file);
+  };
+
+  return (
+    <Container>
+      <Center>
+        <ButtonGroup>
+          <WrapItem>
+            <Button
+              onClick={() => {
+                if (xmlFileElement.current) {
+                  xmlFileElement.current.click();
+                }
+              }}
+              colorScheme="cyan"
+              marginTop={5}
+              // onChange={handleFileChange}
+            >
+              <input
+                type="file"
+                onChange={handleXmlFileChange}
+                style={{ display: "none" }}
+                id="inputXml"
+                accept=".mxl"
+                ref={xmlFileElement}
+              />
+              Load Sheet
+            </Button>
+          </WrapItem>
+          <WrapItem>
+            <Button
+              onClick={() => {
+                if (audioFileElement.current) {
+                  audioFileElement.current.click();
+                }
+              }}
+              colorScheme="cyan"
+              marginTop={5}
+            >
+              <input
+                type="file"
+                onChange={handleAudioFileChange}
+                style={{ display: "none" }}
+                id="inputAudio"
+                accept="audio/*"
+                ref={audioFileElement}
+              />
+              Load Recording
+            </Button>
+          </WrapItem>
+          <WrapItem>
+            <Button colorScheme="cyan" marginTop={5}>
+              <Icon as={Search2Icon}></Icon>
+            </Button>
+          </WrapItem>
+        </ButtonGroup>
+      </Center>
+    </Container>
+  );
+}
+
+// //? XML FILE
+// const pickXml = async () => {
+//   try {
+//     const result = await FilePicker.pickFiles({
+//       types: [".mxl"],
+//     });
+//   } catch (error) {}
+// };
+
+// //? AUDIO FILE
+// const pickAudio = async () => {
+//   try {
+//     const result = await FilePicker.pickFiles({
+//       types: ["audio/*"],
+//     });
+//   } catch (error) {}
+// };
